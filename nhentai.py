6ba0c51f7f3d4528406ef1f1df9eb0261480b195
Maverick Zaytsev <34956153+kuntulsu@users.noreply.github.com>
2020-01-05 13:17:08 +0700

    Update nhentai.py
    Adding resume feature on .download()

--- nhentai.py
+++ nhentai.py
@@ -5,88 +5,94 @@ from urllib.request import urlopen, Request
 import urllib.request
 import os
 
-os.system("cls" if os.name == 'nt' else 'clear')
-#printing main app
-print("nhentai downloader (under development)")
-print("Find your doujin in nhentai.net!")
-print("***********************************")
-print("In This Version you dont need vpn to download doujin , many thanks to nhproxy.glitch.me and nhent.ai for providing api and links")
-print("***********************************")
-print("input nuke code below!")
-print("sst..! 177013 is my recommendation, check it out!")
 
+"""
+IF THIS APPLICATION ALWAYS RETURNING AN ERROR CONTINOUSLY KINDLY CHECK :
+http://nhproxy.glitch.me and
+https://nhent.ai
+AS THIS FAR, WE DOES NOT USE ANY VPN / PROXIES EMBEDDED IN THIS APP SO NEED THOSE WEBSITE RUNNING
+VPN/PROXIES FEATURE MAY BE INTRODUCED IN THE FUTURE UPDATE AND WE'LL USE https://nhentai.net INSTEAD 
+THANK YOU
 
-def inputNumber():
-	while True:
-		try:
-			userinput = int(input("Input The Nuke Code (Code must be numbers): "))  
-		except ValueError:
-			print("The Code must be numbers! Try again.")
-			continue
-		else:
-			return userinput 
-			break 
+example of use :
+====================
+import nhentai
+code = input("Insert Code Here:")
+response = index.get(code)
+#once you didnt get any error after this , youre able to get data like this:
 
-userinput = inputNumber()
-userinput = str(userinput)
-print("Connecting to https://nhentai.net ...")
-print("Collecting Information",userinput,"...")
-book_id = userinput
-# getting information about link submitted
-link = "https://nhproxy.glitch.me/api/gallery/"+book_id
-try:
-	response = json.loads(requests.get(link).text)
-except requests.exceptions.ConnectionError:
-	os.system('cls' if os.name=='nt' else 'clear')
-	print("An Error Occured during connecting to the nhentai.net, maybe your internet went down , or nhentai was blocked by your country")
-	print("Exiting.")
-	quit()
-if "error" in response:
-	os.system('cls' if os.name=='nt' else 'clear')
-	print("404 Not Found")
-	quit()
-
-media_id = response["media_id"]
-title = response["title"]["english"]
-
-links = len(response["images"]["pages"])
-os.system('cls' if os.name=='nt' else 'clear')
-#Showing the information about the book
-print("***********************************")
-print("Title : ",title)
-print("Pages : ",links)
-print("***********************************")
-print("Your Files is Ready to Download  Press Y to Continue or N to Abort!")
-decision = input()
-if decision == "Y" or decision == "y":
+nh_media_id = response['media_id'] #getting media id
+nh_id = response['id'] #getting book id
+nh_title_eng = response['title']['english'] #getting name of doujin on english version
+nh_title_jp = response['title']['japanese'] #getting name of doujin on japanese (if exist)
+=========================================
+"""
+class Error(Exception):
 	pass
-else:
-	print("Abort.")
-	quit()
+class Error404(Error):
+	pass
 
-# main action
-	#getcwd for pathing material.
-cwd = os.getcwd()
+api_link = "http://nhproxy.glitch.me/api/gallery/" 
+page_link = "https://cdn.nhent.ai/galleries/"
+
+default_download_path = str(os.getenv("HOME"))+"/Downloads/"
 try:
-	folder = os.mkdir(str(response["id"]))
-	print("Folder Created in : "+ str(cwd)+"/"+str(response["id"]))
-except:
-	folder = response["id"]
-	print("Folder Already Created in : "+ str(cwd)+"/"+str(folder))
-	print("Continue...")
-fullpath = str(cwd)+"/"+str(response["id"])
+	os.mkdir(default_download_path+"nhentai")
+	default_download_path = str(os.getenv("HOME"))+"/Downloads/nhentai/"
+except FileExistsError:
+	default_download_path = str(os.getenv("HOME"))+"/Downloads/nhentai/"
 
-start = 1
-end = links + 1
-print("Downloading...")
 
-for x in range(start,end):
-	percent = int(x/links* 100)
-	download_link = "https://cdn.nhent.ai/galleries/"+str(media_id)+"/"+str(x)+".jpg"
-	opener = urllib.request.build_opener()
-	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-	urllib.request.install_opener(opener)
-	urllib.request.urlretrieve(download_link, fullpath+"/"+str(x)+".jpg")
-	print(download_link,"[",x,"/",links,"](",percent,"%)")
-#end of main action
-print("Done, you can find your downloaded doujin in"+fullpath)
+def get(code = ""): #getting stuff like doujin name,page count , or error
+	code = int(code)
+	if not isinstance(code, int):
+		raise ValueError("Code Must be Numbers")
+	code  = str(code)
+	if len(code) > 0 :
+		#main code must be executed
+		try:
+			response = json.loads(requests.get(api_link+code).text)
+			if "error" in response:
+				raise Error404("The Code You Entered ... Seems cannot be found (404)")
+			else:
+				return response
+		except requests.exceptions.ConnectionError:
+			raise requests.exceptions.ConnectionError("Error Connecting to: "+api_link+str(code))
+def download(response):
+	links = len(response["images"]["pages"])
+	end = links + 1
+	start = 1
+
+	#excepting for existing downloadable book and been resumed
+	if os.path.isdir(os.path.join(default_download_path+str(response['id']))):
+		num_files = len([f for f in os.listdir(default_download_path+str(response["id"]) )if os.path.isfile(os.path.join(default_download_path+str(response["id"]) , f))])
+		if num_files > 0 :
+			start = num_files
+			os.unlink(default_download_path+str(response["id"])+"/"+str(num_files)+".jpg")
+		else:
+			pass
+		try:
+			for x in range(start,end):
+				percent = int(x/links* 100)
+				download_link = page_link+str(response["media_id"])+"/"+str(x)+".jpg"
+				opener = urllib.request.build_opener()
+				opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+				urllib.request.install_opener(opener)
+				urllib.request.urlretrieve(download_link, default_download_path+"/"+str(response["id"])+"/"+str(x)+".jpg")
+				print(download_link,"["+str(x)+"/"+str(links)+"]("+str(percent)+"%)")
+		except ConnectionResetError:
+			download(response)
+	else:
+		#start new download
+		try:
+			for x in range(start,end):
+				percent = int(x/links* 100)
+				download_link = page_link+str(response["media_id"])+"/"+str(x)+".jpg"
+				opener = urllib.request.build_opener()
+				opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+				urllib.request.install_opener(opener)
+				urllib.request.urlretrieve(download_link, default_download_path+"/"+str(response["id"])+"/"+str(x)+".jpg")
+				print(download_link,"["+str(x)+"/"+str(links)+"]("+str(percent)+"%)")
+		except ConnectionResetError:
+			#resume() in the future
+			pass
